fce2168e69af70da5fcd50083f58496d3e821ca6
Administrator <Administrator@DESKTOP-95RB6IG>
2023-11-12 12:06:09 +0100

    Aggiungere i file di progetto.

---
+++ WasItAcarOrAcatIsaw/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("Inserisci una parola:");
+string inputUtente = Console.ReadLine();
+
+if (IsPalindromo(inputUtente)){
+    Console.WriteLine($"{inputUtente} è un Palindromo");
+}
+else
+{
+    Console.WriteLine($"{inputUtente} non è un Palindromo");
+
+}
+
+bool IsPalindromo (string s)
+{
+    string verifica = new string(s.Where(c => !char.IsWhiteSpace(c)).ToArray()).ToLower();
+
+    return verifica.SequenceEqual(verifica.Reverse());
+}
